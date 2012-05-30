e5e3897a67270d5320da1446a5c475a8db8365c6
Yangming <yangming@appfirst.com>
2012-05-30 17:30:22 -0400

    new python plugin base and mysql plugin

---
+++ mysql.py
@@ -0,0 +1,64 @@
+'''
+Created on May 29, 2012
+
+@author: yangming
+@copyright: appfirst inc.
+'''
+
+import nagios
+import commands
+
+class MySqlChecker(nagios.BaseAnalyst):
+    def __init__(self):
+        super(MySqlChecker, self).__init__()
+        self.parser.add_argument("-t", "--type", type=str, required=True);
+        self.parser.add_argument("-u", "--user", type=str, required=False);
+        self.parser.add_argument("-p", "--password", type=str, required=False);
+
+    def check(self, request):
+        if request.type == 'SLOW_QUERIES':
+            return self.get_slow_queries(request)
+        if request.type == 'CONNECTIONS':
+            return self.get_connections(request)
+
+    def get_slow_queries(self, request):
+        attr = "Slow_queries";
+        service = request.type
+        value = int(self.parse_status_output(request, attr))
+        status_code = self.judge(value, request)
+        r = nagios.Result(service, status_code, '%s slow queries' % value);
+        r.add_performance_data('total', value, warn=request.warn, crit=request.crit)
+
+    def get_connections(self, request):
+        attr = "Connections";
+        service = request.type
+        value = int(self.parse_status_output(request, attr))
+        status_code = self.judge(value, request)
+        r = nagios.Result(service, status_code, '%s connections' % value);
+        r.add_performance_data('total', value, warn=request.warn, crit=request.crit)
+
+    def parse_status_output(self, request, attr):
+        #stats = {}
+        cmd = "mysqladmin"
+        if hasattr(request, "user"):
+            cmd += " --user=%s" % request.user
+        if hasattr(request, "password"):
+            cmd += " --password=%s" % request.password
+        cmd += " extended-status"
+        for l in commands.getoutput(cmd).split('\n')[3:-1]:
+            fields = l.split('|')[1:3]
+            k = fields[0].strip()
+            v = fields[1].strip()
+            if attr == k:
+                return v
+#            stats.setdefault(k, v)
+#            try:
+#                stats[k] = int(v)
+#            except ValueError:
+#                try:
+#                    stats[k] = float(v)
+#                except ValueError:
+#                    pass
+
+if __name__ == "__main__":
+    MySqlChecker().run()
---
+++ nagios.py
@@ -0,0 +1,104 @@
+'''
+Created on May 29, 2012
+
+@author: yangming
+@copyright: appfirst inc.
+'''
+import argparse, sys
+
+class Status(object):
+    OK = 0
+    WARNING = 1
+    CRITICAL = 2
+    UNKNOWN = 3
+
+    @staticmethod
+    def to_status(status_code):
+        if status_code == Status.OK:
+            return 'OK'
+        elif status_code == Status.WARNING:
+            return 'WARNING'
+        elif status_code == Status.CRITICAL:
+            return 'CRITICAL'
+        else:
+            return 'UNKNOWN'
+
+class Result(object):
+    def __init__(self, service, status_code, message):
+        self.service = service.upper()
+        self.status_code = status_code
+        self.status = Status.to_status(status_code)
+        self.message = message
+        self.perf_data_list = []
+
+    def add_performance_data(self, label, value, UOM=None,
+                             warn=None, crit=None, minv=None, maxv=None):
+        perfdata = {'label':label,'value':value,'UOM' :UOM,
+                    'warn' :warn, 'crit' :crit, 'minv' :minv, 'maxv' :maxv}
+        self.perf_data_list.append(perfdata)
+
+    def __str__(self):
+        output = '{0} {1}: {2}'.format(self.service, self.status, self.message)
+        for pd in self.perf_data_list:
+            output += self._get_perfdata_output(pd)
+        return output
+
+    def _get_perfdata_output(self, perfdata):
+        pdline = " | \'{0}\'={1}".format(perfdata["label"], perfdata["value"])
+        if perfdata["UOM"] is not None:
+            pdline += perfdata["UOM"]
+        if perfdata["warn"] is not None:
+            pdline += ';%s' % perfdata["warn"]
+        if perfdata["crit"] is not None:
+            pdline += ';%s' % perfdata["crit"]
+        if perfdata["minv"] is not None:
+            pdline += ';%s' % perfdata["minv"]
+        if perfdata["maxv"] is not None:
+            pdline += ';%s' % perfdata["maxv"]
+        return pdline
+
+class BaseAnalyst(object):
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self._default_argument()
+
+    def _default_argument(self):
+        self.parser.add_argument("-w", "--warn", type=int, required=False)
+        self.parser.add_argument("-c", "--crit", type=int, required=False)
+
+    def _parse_range(self, range_str):
+        pass
+
+    def run(self):
+        self.request = self.parser.parse_args(sys.argv[1:])
+        result = self.check(self.request)
+        print result
+        sys.exit(self.exit_code)
+
+    def check(self, request):
+        service = 'service'
+        value = 2
+        status_code = self.judge(value, request)
+        r = Result(service, status_code, 'services %s instances' % value);
+        label = 'label'
+        UOM = 'c'
+        minv = 0
+        maxv = 10
+        r.add_performance_data(label, value, UOM, request.warn, request.crit, minv, maxv)
+        return r
+
+    def judge(self, value, request):
+        status_code = Status.UNKNOWN
+        if hasattr(request, 'warn') and request.warn is not None and value < request.warn:
+            status_code = Status.OK
+        elif hasattr(request, 'crit') and request.crit is not None and value >= request.crit:
+            status_code = Status.CRITICAL
+        elif (hasattr(request, 'warn') and request.warn is not None):
+            status_code = Status.WARNING
+        else:
+            status_code = Status.OK
+        return status_code
+
+    @property
+    def exit_code(self):
+        return 0
