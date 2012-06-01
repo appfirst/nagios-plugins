e93a9e18ec7d23367073fa8567e8ef2cb6acd8d9
Yangming <yangming@appfirst.com>
2012-06-01 18:09:17 -0400

    redis add new data sources

--- redis.py
+++ redis.py
@@ -3,10 +3,11 @@ Created on May 31, 2012
 
 @author: appfirst
 '''
+import re
 import nagios
 import commands
 
-class RedisChecker(nagios.BasePlugin):
+class RedisChecker(nagios.BatchStatusPlugin):
     def __init__(self):
         super(RedisChecker, self).__init__()
         choices = ["CUR_OPERATIONS_RATE",
@@ -21,25 +22,24 @@ class RedisChecker(nagios.BasePlugin):
         self.parser.add_argument("-p", "--password", type=str, required=False);
 
     def check(self, request):
+        if request.type == 'TOTAL_KEYS':
+            return self.get_total_keys(request)
         self.stats = self.parse_status_output(request)
         if len(self.stats) == 0:
-            return nagios.Result(request.type, nagios.Status.UNKNOWN,
+            return nagios.Result(request.type,nagios.Status.UNKNOWN,
                                  "failed to check redis. check arguments and try again.")
         if request.type == 'CUR_OPERATIONS_RATE':
-            return nagios.Result(request.type, nagios.Status.UNKNOWN,"mysterious status")
+            return self.get_current_operations_rate(request)
         if request.type == 'AVG_OPERATIONS_RATE':
-            return nagios.Result(request.type, nagios.Status.UNKNOWN,"mysterious status")
+            return self.get_average_operations_rate(request)
         if request.type == 'MEMORY_USED':
-            r = self.get_memory_used(request)
+            return self.get_memory_used(request)
         if request.type == 'CHANGES_SINCE_LAST_SAVE':
-            r = self.get_changes_since_last_save(request)
+            return self.get_changes_since_last_save(request)
         if request.type == 'READ_WRITE_RATIO':
-            return nagios.Result(request.type, nagios.Status.UNKNOWN,"mysterious status")
-        if request.type == 'TOTAL_KEYS':
-            return nagios.Result(request.type, nagios.Status.UNKNOWN,"mysterious status")
+            return nagios.Result(request.type,nagios.Status.UNKNOWN,"mysterious status")
         if request.type == 'COMMAND_FREQUENCY':
-            return nagios.Result(request.type, nagios.Status.UNKNOWN,"mysterious status")
-        return r
+            return self.get_command_frequency(request)
 
     def parse_status_output(self, request):
         stats = {}
@@ -59,27 +59,59 @@ class RedisChecker(nagios.BasePlugin):
                     pass
         return stats
 
-    def get_command_frequency(self, request):
+    def get_current_operations_rate(self, request):
         queries = self.get_delta_value("total_commands_processed")
         sec = self.get_delta_value("uptime_in_seconds")
         value = queries / sec
         status_code = self.verdict(value, request)
-        r = nagios.Result(request.type, status_code, '%s queries per second' % value);
+        r = nagios.Result(request.type, status_code, '%s commands per second' % value);
+        r.add_performance_data('frequency', value, warn=request.warn, crit=request.crit)
+        return r
+
+    def get_average_operations_rate(self, request):
+        queries = self.stats["total_commands_processed"]
+        sec = self.stats["uptime_in_seconds"]
+        value = queries / sec
+        status_code = self.verdict(value, request)
+        r = nagios.Result(request.type, status_code, '%s commands per second' % value);
         r.add_performance_data('frequency', value, warn=request.warn, crit=request.crit)
         return r
 
     def get_memory_used(self, request):
-        value = self.stats["used_memory"]
+        value = float(self.stats["used_memory"]) / 1024 / 1024
         status_code = self.verdict(value, request)
-        r = nagios.Result(request.type, status_code, '%s used_memory' % value);
-        r.add_performance_data('changes', value, warn=request.warn, crit=request.crit)
+        r = nagios.Result(request.type, status_code, "%sMB used_memory" % value);
+        r.add_performance_data("used_memory", value, "MB", warn=request.warn, crit=request.crit)
         return r
 
     def get_changes_since_last_save(self, request):
         value = self.stats["changes_since_last_save"]
         status_code = self.verdict(value, request)
-        r = nagios.Result(request.type, status_code, '%s changes since last save' % value);
-        r.add_performance_data('changes', value, warn=request.warn, crit=request.crit)
+        r = nagios.Result(request.type, status_code, "%s changes since last save" % value);
+        r.add_performance_data("changes", value, warn=request.warn, crit=request.crit)
+        return r
+
+    def get_total_keys(self, request):
+        cmd = "redis-cli dbsize"
+        output = commands.getoutput(cmd)
+        dbsize_pattern = re.compile(".*?(\d+)")
+        matchResult = dbsize_pattern.match(output)
+        if not matchResult:
+            return nagios.Result(request.type,nagios.Status.UNKNOWN,
+                                 "failed to check redis. check arguments and try again.")
+        value = int(matchResult.groups(1)[0])
+        status_code = self.verdict(value, request)
+        r = nagios.Result(request.type, status_code, '%s total keys' % value);
+        r.add_performance_data('total_keys', value, warn=request.warn, crit=request.crit)
+        return r
+
+    def get_command_frequency(self, request):
+        queries = self.get_delta_value("total_commands_processed")
+        sec = self.get_delta_value("uptime_in_seconds")
+        value = queries / sec
+        status_code = self.verdict(value, request)
+        r = nagios.Result(request.type, status_code, '%s commands per second' % value);
+        r.add_performance_data('frequency', value, warn=request.warn, crit=request.crit)
         return r
 
 if __name__ == "__main__":
