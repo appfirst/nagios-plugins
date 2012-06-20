84392bbeb7ab5661d7c7ab49c327252542bc5c96
Yangming <yangming@appfirst.com>
2012-06-20 17:15:04 -0400

    reformat of tests, remove thresholds

--- test/test_checkers.py
+++ test/test_checkers.py
@@ -10,6 +10,7 @@ sys.path.append(
 
 import unittest
 from StringIO import StringIO
+import nagios
 
 from check_postgresql import PostgresChecker
 from check_memcached import MemcachedChecker
@@ -19,73 +20,73 @@ from check_redis import RedisChecker
 
 class TestCheckerBase(unittest.TestCase):
     def assert_status(self, argument):
+        # change the standard output to StringIO()
         original_stdout, sys.stdout = sys.stdout, StringIO()
         args = argument.split()
+        # get output and exit_code
+        exit_code = 0
         try:
             self.checker.run(args)
         except SystemExit as se:
             exit_code = se.code
-        out = sys.stdout.getvalue()
+        output = sys.stdout.getvalue()
+        # change the standard output back
         sys.stdout.close()
         sys.stdout = original_stdout
-        success = False
-        print out,
-        for status_code in range(3):
-            if status_code == exit_code:
-                success = True
-#        if exit_code == 3 and "cannot get service status." not in out:
-#            success = True
-        self.assertTrue(success, "cannot get service status.")
+        # print and assert
+        print "\targv:   %s" % argument
+        print "\tstdout: %s" % output
+        self.assertEqual(nagios.Status.to_status(exit_code), "OK")
 
 class TestPostgresChecker(TestCheckerBase):
     def test_check_postgresql(self):
         self.checker = PostgresChecker()
         print ' - testing: check_postgresql'
-        self.assert_status("-w 1000 -c 2000 -t TUPLE_READ")
-        self.assert_status("-w 1000 -c 2000 -t TUPLE_INSERTED")
-        self.assert_status("-w 1000 -c 2000 -t TUPLE_UPDATED")
-        self.assert_status("-w 1000 -c 2000 -t TUPLE_DELETED")
+        self.assert_status("-t TUPLE_READ")
+        self.assert_status("-t TUPLE_INSERTED")
+        self.assert_status("-t TUPLE_UPDATED")
+        self.assert_status("-t TUPLE_DELETED")
 
 class TestMySqlChecker(TestCheckerBase):
     def test_check_mysql(self):
         self.checker = MySqlChecker()
         print ' - testing: check_mysql'
-        self.assert_status("-w 1000 -c 2000 -t QUERIES_PER_SECOND")
-        self.assert_status("-w 1000 -c 2000 -t SLOW_QUERIES")
-        self.assert_status("-w 1000 -c 2000 -t ROW_OPERATIONS")
-        self.assert_status("-w 1000 -c 2000 -t TRANSACTIONS")
-        self.assert_status("-w 1000 -c 2000 -t CONNECTIONS")
-        self.assert_status("-w 1000 -c 2000 -t TOTAL_BYTES")
-        self.assert_status("-w 1000 -c 2000 -t SELECTS")
+        self.assert_status("-t QUERIES_PER_SECOND")
+        self.assert_status("-t SLOW_QUERIES")
+        self.assert_status("-t ROW_OPERATIONS")
+        self.assert_status("-t TRANSACTIONS")
+        self.assert_status("-t CONNECTIONS")
+        self.assert_status("-t TOTAL_BYTES")
+        self.assert_status("-t SELECTS")
 
 class TestMemcachedChecker(TestCheckerBase):
     def test_check_memcached(self):
         self.checker = MemcachedChecker()
         print ' - testing: check_memcached'
-        self.assert_status("-w 1000 -c 2000 -t OPERATIONS_SET_REQUESTS")
-        self.assert_status("-w 1000 -c 2000 -t OPERATIONS_GET_REQUESTS")
-        self.assert_status("-w 1000 -c 2000 -t BYTES_READ")
-        self.assert_status("-w 1000 -c 2000 -t BYTES_WRITTEN")
-        self.assert_status("-w 1000 -c 2000 -t BYTES_ALLOCATED")
-        self.assert_status("-w 1000 -c 2000 -t TOTAL_ITEMS")
-        self.assert_status("-w 1000 -c 2000 -t TOTAL_CONNECTIONS")
+        self.assert_status("-t OPERATIONS_SET_REQUESTS")
+        self.assert_status("-t OPERATIONS_GET_REQUESTS")
+        self.assert_status("-t BYTES_READ")
+        self.assert_status("-t BYTES_WRITTEN")
+        self.assert_status("-t BYTES_ALLOCATED")
+        self.assert_status("-t TOTAL_ITEMS")
+        self.assert_status("-t TOTAL_CONNECTIONS")
 
 class TestRedisChecker(TestCheckerBase):
     def test_check_redis(self):
         self.checker = RedisChecker()
         print ' - testing: check_redis'
-        self.assert_status("-w 1000 -c 2000 -t OPERATIONS_RATE")
-        self.assert_status("-w 1000 -c 2000 -t MEMORY_USED")
-        self.assert_status("-w 1000 -c 2000 -t CHANGES_SINCE_LAST_SAVE")
-        self.assert_status("-w 1000 -c 2000 -t TOTAL_KEYS")
+        self.assert_status("-t OPERATIONS_RATE")
+        self.assert_status("-t MEMORY_USED")
+        self.assert_status("-t CHANGES_SINCE_LAST_SAVE")
+        self.assert_status("-t TOTAL_KEYS")
 
 class TestPassengerChecker(TestCheckerBase):
     def test_check_passenger(self):
         self.checker = PassengerChecker()
         print ' - testing: check_passenger'
-        self.assert_status("-w 1000 -c 2000 -t MAX_PROCESSES")
-        self.assert_status("-w 1000 -c 2000 -t RUNNING_PROCESSES")
-        self.assert_status("-w 1000 -c 2000 -t ACTIVE_PROCESSES")
+        self.assert_status("-t MAX_PROCESSES")
+        self.assert_status("-t RUNNING_PROCESSES")
+        self.assert_status("-t ACTIVE_PROCESSES")
 
 class TestMongoDBChecker(TestCheckerBase):
     def test_check_mongodb(self):
