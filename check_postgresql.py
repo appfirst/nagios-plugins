c78c0cf57d0ec3977901ab507c0cb03543bea200
Yangming <yangming@appfirst.com>
2012-06-20 16:48:59 -0400

    check_postgresql read from pg_stat_database instead of using functions

--- check_postgresql.py
+++ check_postgresql.py
@@ -9,89 +9,90 @@ from nagios import BatchStatusPlugin
 import commands
 
 class PostgresChecker(BatchStatusPlugin):
-    def __init__(self):
-        super(PostgresChecker, self).__init__()
+    def __init__(self, *args, **kwargs):
+        super(PostgresChecker, self).__init__(*args, **kwargs)
         self.parser.add_argument("-f", "--filename", default='psql', type=str, required=False);
 
     #TODO: replace all with "psql -c 'select * from pg_stat_database' -A" 
 
+    def retreive_current_status(self, request, colname):
+        sql_stmt = "select datname, %s from pg_stat_database;" % colname
+        sub_stats = {}
+        rows = self.run_sql(sql_stmt)
+        if not len(rows):
+            return sub_stats
+        for datname, value in rows:
+            try:
+                value = int(value)
+            except ValueError:
+                pass
+            sub_stats[datname] = value
+        return sub_stats
+
     @BatchStatusPlugin.command("TUPLE_READ")
     def get_tuple_read(self, request):
-        sql_stmt = "select datid, datname, tup_fetched from pg_stat_database"
-        oids = self.run_sql(sql_stmt)
-        value = 0
-        for oid in oids:
-            sql_stmt = "select * from pg_stat_get_tuples_fetched(%s)" % oid
-            row = self.run_sql(sql_stmt)[0]
-            try:
-                value += int(row)
-            except ValueError:
-                return nagios.Result(request.type,nagios.Status.UNKNOWN,
-                                     "failed to check postgres. check arguments and try again.")
+        sub_stats = self.retreive_current_status(request, "tup_fetched")
+        if len(sub_stats) == 0:
+            return nagios.Result(request.type,nagios.Status.CRITICAL,
+                                "failed to check postgres. check arguments and try again.")
+        value = reduce(lambda x,y:x+y, sub_stats.itervalues())
         status_code = self.verdict(value, request)
         r = nagios.Result(request.type, status_code, '%s tuple fetched' % value);
-        r.add_performance_data('tuple_fetched', value, warn=request.warn, crit=request.crit)
+        r.add_performance_data('total', value, warn=request.warn, crit=request.crit)
+        for k, v in sub_stats.iteritems():
+            r.add_performance_data(k, v, warn=request.warn, crit=request.crit)
         return r
 
     @BatchStatusPlugin.command("TUPLE_INSERTED")
     def get_tuple_inserted(self, request):
-        sql_stmt = "select oid from pg_database"
-        oids = self.run_sql(sql_stmt)
-        value = 0
-        for oid in oids:
-            sql_stmt = "select * from pg_stat_get_tuples_inserted(%s)" % oid
-            row = self.run_sql(sql_stmt)[0]
-            try:
-                value += int(row)
-            except ValueError:
-                return nagios.Result(request.type,nagios.Status.UNKNOWN,
-                                     "failed to check postgres. check arguments and try again.")
+        sub_stats = self.retreive_current_status(request, "tup_inserted")
+        if len(sub_stats) == 0:
+            return nagios.Result(request.type,nagios.Status.CRITICAL,
+                                "failed to check postgres. check arguments and try again.")
+        value = reduce(lambda x,y:x+y, sub_stats.itervalues())
         status_code = self.verdict(value, request)
         r = nagios.Result(request.type, status_code, '%s tuple inserted' % value);
-        r.add_performance_data('tuple_inserted', value, warn=request.warn, crit=request.crit)
+        r.add_performance_data('total', value, warn=request.warn, crit=request.crit)
+        for k, v in sub_stats.iteritems():
+            r.add_performance_data(k, v, warn=request.warn, crit=request.crit)
         return r
 
     @BatchStatusPlugin.command("TUPLE_UPDATED")
     def get_tuple_updated(self, request):
-        sql_stmt = "select oid from pg_database"
-        oids = self.run_sql(sql_stmt)
-        value = 0
-        for oid in oids:
-            sql_stmt = "select * from pg_stat_get_tuples_updated(%s)" % oid
-            row = self.run_sql(sql_stmt)[0]
-            try:
-                value += int(row)
-            except ValueError:
-                return nagios.Result(request.type,nagios.Status.UNKNOWN,
-                                     "failed to check postgres. check arguments and try again.")
+        sub_stats = self.retreive_current_status(request, "tup_updated")
+        if len(sub_stats) == 0:
+            return nagios.Result(request.type,nagios.Status.CRITICAL,
+                                "failed to check postgres. check arguments and try again.")
+        value = reduce(lambda x,y:x+y, sub_stats.itervalues())
         status_code = self.verdict(value, request)
         r = nagios.Result(request.type, status_code, '%s tuple updated' % value);
-        r.add_performance_data('tuple_updated', value, warn=request.warn, crit=request.crit)
+        r.add_performance_data('total', value, warn=request.warn, crit=request.crit)
+        for k, v in sub_stats.iteritems():
+            r.add_performance_data(k, v, warn=request.warn, crit=request.crit)
         return r
 
     @BatchStatusPlugin.command("TUPLE_DELETED")
     def get_tuple_deleted(self, request):
-        sql_stmt = "select oid from pg_database"
-        oids = self.run_sql(sql_stmt)
-        value = 0
-        for oid in oids:
-            sql_stmt = "select * from pg_stat_get_tuples_deleted(%s)" % oid
-            row = self.run_sql(sql_stmt)[0]
-            try:
-                value += int(row)
-            except ValueError:
-                return nagios.Result(request.type,nagios.Status.UNKNOWN,
-                                     "failed to check postgres. check arguments and try again.")
+        sub_stats = self.retreive_current_status(request, "tup_deleted")
+        if len(sub_stats) == 0:
+            return nagios.Result(request.type,nagios.Status.CRITICAL,
+                                "failed to check postgres. check arguments and try again.")
+        value = reduce(lambda x,y:x+y, sub_stats.itervalues())
         status_code = self.verdict(value, request)
         r = nagios.Result(request.type, status_code, '%s tuple deleted' % value);
-        r.add_performance_data('tuple_deleted', value, warn=request.warn, crit=request.crit)
+        r.add_performance_data('total', value, warn=request.warn, crit=request.crit)
+        for k, v in sub_stats.iteritems():
+            r.add_performance_data(k, v, warn=request.warn, crit=request.crit)
         return r
 
     def run_sql(self, sql_stmt):
         cmd_template = "psql -c \'%s\' -A -t"
         cmd = cmd_template % sql_stmt
         output = commands.getoutput(cmd)
+        if "command not found" in output:
+            return []
         return [row.split('|') for row in output.split("\n")]
 
 if __name__ == "__main__":
-    PostgresChecker().run()
+    import sys
+    PostgresChecker().run(sys.argv[1:])
